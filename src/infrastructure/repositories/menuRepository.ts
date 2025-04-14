f9abd2c168f942b9be578966602bcf3aa08f364b
KevinRivera1 <shimpo11.kr@gmail.com>
2025-04-14 18:16:02 -0500

    refactor(menu): update Menu interface and improve getAll method for type safety

--- src/infrastructure/repositories/menuRepository.ts
+++ src/infrastructure/repositories/menuRepository.ts
@@ -1,7 +1,6 @@
 import MenuData from '@data/Menu.json';
+import type { Menu } from '@domain/menu';
 
 export const getMenuRepository = {
-	getAll: async () => {
-		return MenuData;
-	},
+	getAll: async (): Promise<Menu> => Promise.resolve(MenuData),
 };
