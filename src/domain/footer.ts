79c383321996fd323ca02ef961b420fcc3dd92c7
KevinRivera1 <shimpo11.kr@gmail.com>
2025-04-14 14:53:12 -0500

    style(footer): format code for consistency and readability

--- src/domain/footer.ts
+++ src/domain/footer.ts
@@ -1,49 +1,49 @@
 export interface Footer {
-    footer: FooterClass;
+	footer: FooterClass;
 }
 
 export interface FooterClass {
-    content: Content;
+	content: Content;
 }
 
 export interface Content {
-    logo:        Logo;
-    sections:    Section[];
-    socialMedia: SocialMedia;
-    copyright:   Copyright;
+	logo: Logo;
+	sections: Section[];
+	socialMedia: SocialMedia;
+	copyright: Copyright;
 }
 
 export interface Copyright {
-    text: string;
+	text: string;
 }
 
 export interface Logo {
-    link:      string;
-    component: string;
+	link: string;
+	component: string;
 }
 
 export interface Section {
-    title: string;
-    links: SectionLink[];
+	title: string;
+	links: SectionLink[];
 }
 
 export interface SectionLink {
-    text: string;
-    url:  URL;
+	text: string;
+	url: URL;
 }
 
 export enum URL {
-    Empty = "#",
+	Empty = '#',
 }
 
 export interface SocialMedia {
-    title: string;
-    links: SocialMediaLink[];
+	title: string;
+	links: SocialMediaLink[];
 }
 
 export interface SocialMediaLink {
-    platform:      string;
-    url:           URL;
-    iconComponent: string;
-    ariaLabel:     string;
+	platform: string;
+	url: URL;
+	iconComponent: string;
+	ariaLabel: string;
 }
