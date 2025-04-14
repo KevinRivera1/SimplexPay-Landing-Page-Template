231bb916f83744fe2d7187b1ee45a094dc755876
KevinRivera1 <shimpo11.kr@gmail.com>
2025-04-14 14:12:05 -0500

    refactor(footer): clean up commented-out code in footer repository

--- src/infrastructure/repositories/footerRepository.ts
+++ src/infrastructure/repositories/footerRepository.ts
@@ -1,9 +1,5 @@
 import FooterData from '@data/Footer.json';
 
-//export const getFooterDataRepository = async () => {
-//	return  FooterData.footer.content;
-//};
-
 export const getFooterDataRepository = {
 	getAll: async () => {
 		return FooterData.footer.content;
