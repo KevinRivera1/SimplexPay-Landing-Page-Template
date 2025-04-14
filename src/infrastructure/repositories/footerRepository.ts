4f36301872b9b4c813b090f4566e312d1227ebba
KevinRivera1 <shimpo11.kr@gmail.com>
2025-04-14 18:15:57 -0500

    fix(footer): format getAll method for improved readability

--- src/infrastructure/repositories/footerRepository.ts
+++ src/infrastructure/repositories/footerRepository.ts
@@ -2,5 +2,6 @@ import FooterData from '@data/Footer.json';
 import type { Content } from '@domain/footer';
 
 export const getFooterDataRepository = {
-	getAll: async (): Promise<Content> => Promise.resolve(FooterData.footer.content)
+	getAll: async (): Promise<Content> =>
+		Promise.resolve(FooterData.footer.content),
 };
